c3d835b7193127449f089d52747c6e916fb344c4
Alexey Kalyakin <paxclub@yandex.ru>
2022-03-18 16:25:07 +0300

    added junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
@@ -12,39 +12,43 @@ public class CashbackHackServiceTest {
         int actual = service.remain(0);
         int expected = 1000;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
+
     @Test
     public void testRemain1() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1);
         int expected = 999;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
+
     @Test
     public void testRemain2() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(500);
         int expected = 500;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
+
     @Test
     public void testRemain3() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
         int expected = 0;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
+
     @Test
     public void testRemain4() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(2000);
         int expected = 1000;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
 
 }
